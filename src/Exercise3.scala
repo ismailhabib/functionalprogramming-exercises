c6b9015fc657681d416a720e8c7de1e7d3b96b4f
Ismail Habib Muhammad <ismail.muhammad@mendix.com>
2016-03-01 22:59:10 +0100

    Exercise 3.22-3.24

--- src/Exercise3.scala
+++ src/Exercise3.scala
@@ -126,6 +126,44 @@ object List {
   def filter2[A](as: List[A])(f: A => Boolean): List[A] = flatMap(as)(x => if (f(x)) List(x) else Nil)
 
   def filterOdd2(as: List[Int]): List[Int] = filter2(as)(x => x % 2 == 0)
+
+  //Exercise 3.22
+  def addList(ints: List[Int], ints2: List[Int]): List[Int] = ints match {
+    case Nil => Nil
+    case Cons(x, xs) => ints2 match {
+      case Nil => Nil
+      case Cons(y, ys) => Cons(x + y, addList(xs, ys))
+    }
+  }
+
+  //Exercise 3.23
+  def zipWith[A, B, C](l1: List[A], l2: List[B])(f: (A, B) => C): List[C] = l1 match {
+    case Nil => Nil
+    case Cons(x, xs) => l2 match {
+      case Nil => Nil
+      case Cons(y, ys) => Cons(f(x, y), zipWith(xs, ys)(f))
+    }
+  }
+
+  //Exercise 3.24
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = {
+    def rec(sup: List[A], pSub:List[A]): Boolean = {
+      sup match {
+        case Nil => pSub match {
+          case Nil => true
+          case Cons(y, ys) => false
+        }
+        case Cons(x, xs) => pSub match {
+          case Nil => true
+          case Cons(y, ys) if (x == y) => rec(xs, ys) || hasSubsequence(xs, sub)
+          case Cons(y, ys) => rec(xs, sub)
+        }
+      }
+    }
+    rec(sup, sub)
+  }
+
+
 }
 
 object Exercise3 {
@@ -146,5 +184,21 @@ object Exercise3 {
     println("Filter odd: " + List.filterOdd(List(8, 3, 2, 5, 7, 4, 4)))
     println("Flat Map x,x+1: " + List.flatMap(List(1, 2, 3))(x => List(x, x + 1)))
     println("Filter odd 2: " + List.filterOdd2(List(8, 3, 2, 5, 7, 4, 4)))
+    println("Add 2 list: " + List.addList(List(5, 4, 3), List(5, 10, 15)))
+    println("Zip with: " + List.zipWith(List(5, 4, 3), List(5, 10, 15))((x, y) => x + y))
+
+    val list1 = List(1, 2, 3, 4, 5)
+    val list2 = List(1, 2)
+    val list3 = List(3, 4)
+    val list4 = List(5, 6)
+    val list5 = List(1, 3)
+    val list6 = List(2)
+
+    println(List.hasSubsequence(list1, list1))
+    println(List.hasSubsequence(list1, list2))
+    println(List.hasSubsequence(list1, list3))
+    println(List.hasSubsequence(list1, list4))
+    println(List.hasSubsequence(list1, list5))
+    println(List.hasSubsequence(list1, list6))
   }
 }
