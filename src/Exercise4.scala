688b6cc5290585781c85d10a9a5504f44be650f8
Ismail Habib Muhammad <ismail.muhammad@mendix.com>
2016-03-19 02:54:51 +0100

    Exercise 4

---
+++ src/Exercise4.scala
@@ -0,0 +1,128 @@
+/**
+  * Created by imu on 3/8/2016.
+  */
+
+
+case class Some[+A](get: A) extends Option[A]
+
+case object None extends Option[Nothing]
+
+sealed trait Option[+A] {
+
+  //Exercise 4.1
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(a) => Some(f(a))
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] = this.map(f).getOrElse(None)
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(a) => a
+  }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this.map(Some(_)).getOrElse(ob)
+
+  def filter(f: A => Boolean): Option[A] = this.flatMap(a => if (f(a)) Some(a) else None)
+}
+
+object Option {
+  //Exercise 4.2
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
+
+  //Exercise 4.3
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = (a, b) match {
+    case (None, _) | (_, None) => None
+    case (Some(x), Some(y)) => Some(f(x, y))
+  }
+
+  def map22[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a.flatMap(x => b.map(y => f(x, y)))
+
+  //Exercise 4.4
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case Cons(head, tail) => head.flatMap(h => sequence(tail).map(Cons(h, _)))
+  }
+
+  //Exercise 4.5
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    a match {
+      case Nil => Some(Nil)
+      case Cons(h, t) => map2(f(h), traverse(t)(f))(Cons(_, _))
+    }
+
+  def sequenceViaTraverse[A](a: List[Option[A]]): Option[List[A]] =
+    traverse(a)(x => x)
+}
+
+//Either
+
+case class Left[+E](get: E) extends Either[E, Nothing]
+
+case class Right[+A](get: A) extends Either[Nothing, A]
+
+sealed trait Either[+E, +A] {
+  //Exercise 4.6
+
+  def map[B](f: A => B): Either[E, B] =
+    this match {
+      case Right(a) => Right(f(a))
+      case Left(e) => Left(e)
+    }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Left(e) => Left(e)
+      case Right(a) => f(a)
+    }
+
+  def orElse[EE >: E, AA >: A](b: => Either[EE, AA]): Either[EE, AA] =
+    this match {
+      case Left(_) => b
+      case Right(a) => Right(a)
+    }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = for {a <- this; b1 <- b} yield f(a, b1)
+}
+
+object Either {
+  def mean(xs: IndexedSeq[Double]): Either[String, Double] =
+    if (xs.isEmpty)
+      Left("mean of empty list!")
+    else
+      Right(xs.sum / xs.length)
+
+  def safeDiv(x: Int, y: Int): Either[Exception, Int] =
+    try Right(x / y)
+    catch {
+      case e: Exception => Left(e)
+    }
+
+  def Try[A](a: => A): Either[Exception, A] =
+    try Right(a)
+    catch {
+      case e: Exception => Left(e)
+    }
+
+  def traverse[E, A, B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    es match {
+      case Nil => Right(Nil)
+      case Cons(h, t) => (f(h) map2 traverse(t)(f)) (Cons(_, _))
+    }
+
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
+    traverse(es)(x => x)
+
+}
+
+object Exercise4 extends App {
+
+  println(Option.variance(List(1,2,3,4,5)))
+}
